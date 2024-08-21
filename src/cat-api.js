3382bc0b8a34540851ff5196072169a1d33baf06
unknown <poputa.alex@gmail.com>
2024-08-21 16:24:19 +0300

    finished drop-down

---
+++ src/cat-api.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import axios from 'axios';
+
+const API_KEY = process.env.API_KEY;
+export const MY_URL = process.env.MY_URL;
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+const breedSelector = document.querySelector('.breed-select');
+
+export const fetchBreeds = (url) => {
+  axios
+    .get(url)
+    .then(res => {
+      const { data } = res;
+      if (data) {
+        document.querySelector('.loader').innerText =
+          'Please use the drop-down to select a cat';
+        document.querySelector('.error').style.display = 'none';
+      }
+      data.forEach(cat => {
+        const { name, id } = cat;
+        const breed = `<option value=${id}>${name}</option>`;
+        breedSelector.insertAdjacentHTML('beforeend', breed);
+      });
+    })
+    .catch(err => err.log);
+};
+//   breedSelector.addEventListener('change', e => {
+//     const selectedId = e.currentTarget.value;
+//     const {image, name, temperament, description} = data.find(cat=>cat.id = selectedId);
+//     console.log(name, image, temperament, description);
+//    });
