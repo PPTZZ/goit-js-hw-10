b15e28015e798cc3ab2a16830dae62380bfc6b47
unknown <poputa.alex@gmail.com>
2024-08-22 16:22:40 +0300

    finished getting data

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,33 +1,33 @@
-
 import axios from 'axios';
 
 const API_KEY = 'live_wNJpweNeziX3slnvZae47Wje0uZ6pLfqHHL3cRFFZICVCl0aShYNsGStRonHoK8R';
-export const MY_URL = 'https://api.thecatapi.com/v1/breeds';
+const ENDPOINT = 'https://api.thecatapi.com/v1';
 
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-const breedSelector = document.querySelector('.breed-select');
-
-export const fetchBreeds = (url) => {
-  axios
-    .get(url)
-    .then(res => {
-      const { data } = res;
-      if (data) {
-        document.querySelector('.loader').innerText =
-          'Please use the drop-down to select a cat';
-        document.querySelector('.error').style.display = 'none';
-      }
-      data.forEach(cat => {
-        const { name, id } = cat;
-        const breed = `<option value=${id}>${name}</option>`;
-        breedSelector.insertAdjacentHTML('beforeend', breed);
-      });
-    })
-    .catch(err => err.log);
-};
-//   breedSelector.addEventListener('change', e => {
-//     const selectedId = e.currentTarget.value;
-//     const {image, name, temperament, description} = data.find(cat=>cat.id = selectedId);
-//     console.log(name, image, temperament, description);
-//    });
+export default class CatList {
+  constructor(selector, url) {
+    this.element = this.select(selector);
+  }
+  select(selector) {
+    return document.querySelector(selector);
+  }
+  async getCats() {
+    try {
+      const { data } = await axios.get(`${ENDPOINT}/breeds`);
+      return data;
+    } catch (err) {
+      return console.error(err);
+    }
+  }
+  async getCatInfo(breedId) {
+    try {
+      const { data } = await axios.get(
+        `${ENDPOINT}/images/search?breed_ids=${breedId}`
+      );
+      return data;
+    } catch (err) {
+      return console.error(err);
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,63 @@
-import { MY_URL,fetchBreeds } from "./cat-api.js";
+import CatList from './cat-api.js';
 
-fetchBreeds(MY_URL)
-console.log(MY_URL);
+const selector = new CatList('.breed-select');
+const display = new CatList('.cat-info');
+
+
+const addCSS = css =>
+  (document.head.appendChild(document.createElement('style')).innerHTML = css);
+
+const fetchBreeds = () => {
+  // Getting the cat list
+  selector
+    .getCats()
+    .then(data => {
+      if (data) {
+        // Checking to see if there is any data
+        document.querySelector('.loader').style.display='none'
+        document.querySelector('.error').style.display = 'none';
+      }
+      const placeHolder = '<option value="" selected>Please select a cat</option>';
+      selector.element.insertAdjacentHTML('afterbegin', placeHolder);
+      data.forEach(cat => {
+        // Adding data from axios to selector
+        const { name, id } = cat;
+        const breed = `<option class="option" value="${id}">${name}</option>`;
+        selector.element.insertAdjacentHTML('beforeend', breed);
+      });
+    })
+    .catch(err => console.error(err));
+};
+
+const fetchCatByBreed = id => {
+  selector
+    .getCatInfo(id)
+    .then(data => {
+      const [
+        {
+          url,
+          breeds: [{ name, description, temperament }]
+        }
+      ] = data; // Destructuring data recived from axios
+      const displayedCat = `<img src="${url}" alt="${name}" height="auto" width="500px"><div class="text-box">
+        <h1 class="text"> ${name}</h1>
+        <h2 class="text"> ${temperament}</h2>
+        <p class="text"> ${description}</p></div>`;
+      display.element.insertAdjacentHTML('afterbegin', displayedCat);
+    })
+    .catch(err => console.log(err));
+};
+
+const resetDisplay = () => (display.element.innerHTML = '');
+
+fetchBreeds();
+selector.element.addEventListener('change', e => {
+  const selectedCat = e.currentTarget.value;
+  resetDisplay();
+  fetchCatByBreed(selectedCat);
+});
+
+addCSS('.breed-select{font-size:40px; font-family:Arial; text-align:center; color:charcoal; border-radius:5px}');
+addCSS('.text{font-family:Arial; max-width:500px}');
+addCSS('.cat-info {display:flex; justify-content:center; align-items:flex-end; gap:50px; margin-top:30px}');
+addCSS('.text-box{display:flex; flex-flow:column; justify-content:flex-end;}')
